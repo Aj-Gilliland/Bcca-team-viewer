cda05897fe845763e5b20d9f62d777a8f6d78de1
AJ Gilliland <manuelgillilandiv@gmail.com>
2023-11-01 15:17:51 -0500

    everything lookin snazzy

--- app/views.py
+++ app/views.py
@@ -6,78 +6,52 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Team:
-    name: str           #team name
-    description: str    #what the team exist for
-    duty: str           #what they do in their day to day
-    people: list[str]   #people on team
+class Project:
+    name: str           #project name
+    description: str    #what the thing does
+    people: list[str]   #people who worked on the project
+    link: str           #link to github repo
+
+
+#['Darrell','Conner','Kaleigh','Blair','Wyatt','Mina','Jay','Joshua']
+#['Jordan','Joby','Aj','Micah','Caleb']
+#for creating data
 
 
 
-
-
-
-
-
-def everyoneHere (request :HttpRequest)->render:
-    context = {'teams':[
-    Team( 
-        'Community', 
-        'The Community Team is tasked with planning and organizing bi-weekly events, celebrating birthdays, and coordinating school visits to external educational institutions. They are responsible for creating a positive and engaging environment for the school community, ensuring that everyone can enjoy their time at BCCA.',
-        ' Plans bi-weekly events, birthdays, and school visits.',
-        ['Jordan','Joby','Aj','Micah','Caleb'] ),
-
-    Team( 
-        'Documentation', 
-        'Documents events at bcca ,with access to the media accounts and social pages.',
-        'Film events, post, and interview students.',
-        ['Darrell','Conner','Kaleigh','Blair','Wyatt','Mina','Jay','Joshua'] ),
-
-    Team( 
-        'Management', 
-        'Hands out work loads and makes efforts on maintaining the student body.',
-        'Oversee chores and supply levels',
-        ['Owen','Jeremiah','Nick','Ab','Abigail'] ),
-
-    Team( 
-        'Procurement', 
-        'Accumulates supply for things such as lunch, building maintenance, and events. ',
-        'Order in bulk, drive to stores ,set out/plan lunch.',
-        ['Kayleah','Adrian','Bryce','Big John','Blaine'] )
+def homePage (request :HttpRequest)->render:
+    context = {'projects':[
+        Project(
+            'portfolio',
+            'A collection of all of our project and our star worker.',
+            ['Owen','Jeremiah','Nick','Ab','Abigail'],
+            'https://github.com/Aj-Gilliland/Portfolio'),
+        Project(
+            'BCCA team viewer',
+            'This is a collection of information of our different teams and what they do.',
+            ['Kayleah','Adrian','Bryce','Big John','Blaine'],
+            'https://github.com/Aj-Gilliland/Bcca-team-viewer')
     ]}
-    return render(request, "teamList.html", context)
+    return render(request, "home.html", context)
 
-def details (request:HttpRequest, teamNum:int )->render:
-    teams = [
-    Team( 
-        'Community', 
-        'Provides a good environment and the occasional fun time for the whole school.',
-        ' Plans bi-weekly events, birthdays, and school visits.',
-        ['Jordan','Joby','Aj','Micah','Caleb'] ),
-
-    Team( 
-        'Documentation', 
-        'Documents events at bcca ,with access to the media accounts and social pages.',
-        'Film events, post, and interview students.',
-        ['Darrell','Conner','Kaleigh','Blair','Wyatt','Mina','Jay','Joshua'] ),
-
-    Team( 
-        'Management', 
-        'Hands out work loads and makes efforts on maintaining the student body.',
-        'Oversee chores and supply levels',
-        ['Owen','Jeremiah','Nick','Ab','Abigail'] ),
-
-    Team( 
-        'Procurement', 
-        'Accumulates supply for things such as lunch, building maintenance, and events. ',
-        'Order in bulk, drive to stores ,set out/plan lunch.',
-        ['Kayleah','Adrian','Bryce','Big John','Blaine'] )
+def details (request:HttpRequest, projectNum:int )->render:
+    bigList = [
+        Project(
+            'portfolio',
+            'A collection of all of our project and our star worker.',
+            ['Owen','Jeremiah','Nick','Ab','Abigail'],
+            'https://github.com/Aj-Gilliland/Portfolio'),
+        Project(
+            'BCCA team viewer',
+            'This is a collection of information of our different teams and what they do.',
+            ['Kayleah','Adrian','Bryce','Big John','Blaine'],
+            'https://github.com/Aj-Gilliland/Bcca-team-viewer')
     ]
 
     context = {
-        'team':teams[teamNum-1]}
+        'project':bigList[projectNum-1]}
     
-    return render(request, "details.html", context)
+    return render(request, "secondary.html", context)
 
 
 
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import everyoneHere, details
+from app.views import homePage, details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', everyoneHere, name='home'),
-    path('findDetails/<int:teamNum>', details, name='findDetails')
+    path('', homePage, name='home'),
+    path('findDetails/<int:projectNum>', details, name='findDetails')
 ]
   
